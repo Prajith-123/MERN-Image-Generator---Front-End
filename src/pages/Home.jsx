57e87930532057e56bceea2051e36dda5005be99
Prajith Kumar <Kumarprajith944@gmail.com>
2023-03-28 07:53:04 +0530

    First Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://open-ai-dall-e.onrender.com7200/api/v1/post', {
+      const response = await fetch('https://open-ai-dall-e.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
